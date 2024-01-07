79c639e191f6a1063925c5793adf9f4ffedc46b0
alerest285 <81980735+alerest285@users.noreply.github.com>
2024-01-07 13:05:12 +0100

    Add declaration of ServoArm class

--- src/servo_arm.h
+++ src/servo_arm.h
@@ -3,6 +3,42 @@
 
 namespace robotic_arm {
 
+class ServoArm {
+
+  const String _name; 
+  const double _length;
+  const int _pin;
+  const LoggingCallback _logging;
+  const Servo _servo;
+  
+  struct MapRange {
+    double lower_bound;
+    double upper_bound;
+    double servo_to_lower_bound;
+    double servo_to_upper_bound;
+  };
+  const MapRange _map_range;
+    
+  double _current_angle;
+  bool _is_current_angle_set;
+
+  double _transformArmAngleToServoAngle(double angle);
+
+  int _nearestIntegerAngle(double angle);
+
+  String _rangeToString();
+
+  public:
+
+    ServoArm(String name, double length, int pin, MapRange map_range, LoggingCallback logging_callback);
+
+    void moveTo(double angle);
+
+    void moveBy(double delta);
+
+    double currentAngle();
+};
+
 } // namespace robotic_arm
 
 #endif // ROBOTIC_ARM_SERVO_ARM_H
