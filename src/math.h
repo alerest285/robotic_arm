bee21483825a1f5dc10d60857b3a5469f503c7bd
alerest285 <81980735+alerest285@users.noreply.github.com>
2024-01-07 12:59:07 +0100

    Add functions interface in math.h

--- src/math.h
+++ src/math.h
@@ -3,6 +3,24 @@
 
 namespace robotic_arm {
 
+constexpr double pi = 3.1415926535897932384626433;
+
+double cosDegrees(double x);
+
+double cosDegreesDerivative(double x);
+
+double sinDegrees(double x);
+
+double sinDegreesDerivative(double x);
+
+double acosDegrees(double x);
+
+struct PlaneCartesianCoordinates {
+  double x;
+  double y;
+  String toString();
+};
+
 } // namespace robotic_arm
 
 #endif // ROBOTIC_ARM_MATH_H
