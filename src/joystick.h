be7f1f1a16d57578c0069966542df206207f04b3
alerest285 <81980735+alerest285@users.noreply.github.com>
2024-01-07 23:05:24 +0100

    Rename Joystick by CartesianJoystick

--- src/joystick.h
+++ src/joystick.h
@@ -3,7 +3,7 @@
 
 namespace robotic_arm {
 
-class Joystick {
+class CartesianJoystick {
 
   int _horizontal_input_pin;
   int _vertical_input_pin;
@@ -14,9 +14,19 @@ class Joystick {
   const int _max_joyistick_input = 1023;
 
   public: 
-    
-    Joystick(int horizontal_input_pin, int vertical_input_pin, double max_displacement_per_loop);
 
+    /**
+     * @brief Constructs a new MyClass instance.
+     * 
+     * This constructor initializes the MyClass object with provided initial values. 
+     * It sets up the initial state required for the object to function correctly.
+     *
+     * @param horizontal_input_pin Pin number for the horizontal input signal.
+     * @param vertical_input_pin Pin number for the horizontal input signal. 
+     * @param max_displacement_per_loop Maximum displacement signaled (in centimeters).
+     */
+    Joystick(int horizontal_input_pin, int vertical_input_pin, double max_displacement_per_loop);
+    
     PlaneCartesianCoordinates getDeltaCartesianCoordinates();
 };
 
