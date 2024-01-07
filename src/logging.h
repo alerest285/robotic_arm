1f2b5793da43ef933a23eeea8ab3df62e16a0158
alerest285 <81980735+alerest285@users.noreply.github.com>
2024-01-07 12:54:40 +0100

    Add logging artifacts

--- src/logging.h
+++ src/logging.h
@@ -3,6 +3,21 @@
 
 namespace robotic_arm {
 
+enum class LoggingEnum {FATAL, ERROR, WARN, INFO, DEBUG};
+
+typedef void (*LoggingCallback)(LoggingEnum, String);
+
+String LoggingEnumToString(LoggingEnum level) {
+    switch (level) {
+        case LoggingEnum::DEBUG: return "DEBUG";
+        case LoggingEnum::INFO:  return "INFO";
+        case LoggingEnum::WARN:  return "WARN";
+        case LoggingEnum::ERROR: return "ERROR";
+        case LoggingEnum::FATAL: return "FATAL";
+        default:    return "UNKNOWN";
+    }
+}
+
 } // namespace robotic_arm
 
 #endif // ROBOTIC_ARM_LOGGING_H
