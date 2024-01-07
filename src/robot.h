0a9888ccf36e583ce6ef7a8c899720a900d89736
alerest285 <81980735+alerest285@users.noreply.github.com>
2024-01-07 13:18:01 +0100

    Include declaracion of RobotWithHorizontalHand

--- src/robot.h
+++ src/robot.h
@@ -1,7 +1,74 @@
 #ifndef ROBOTIC_ARM_ROBOT_H
 #define ROBOTIC_ARM_ROBOT_H
 
+#include "logging.h"
+#include "math.h"
+
 namespace robotic_arm {
+
+class RobotWithHorizontalHand {
+
+  LoggingCallback _logging;
+
+  const ServoArm *_shoulder;
+  const ServoArm *_elbow;
+  const ServoArm *_hand;
+
+  double _forearm_length;
+
+  const double _differential_stability_threshold = 1e-3;
+
+  struct AngularCoordinates {
+    double shoulder_angle;
+    double elbow_angle;
+    String toString();
+  };
+
+  struct AngularDerivatives {
+    double x_by_shoulder_angle;
+    double x_by_elbow_angle;
+    double y_by_shoulder_angle;
+    double y_by_elbow_angle;    
+  };
+
+  enum class MethodEnum {
+    EXACT, 
+    DERIVATIVE
+  };
+
+  MethodEnum _method;
+  
+  PlaneCartesianCoordinates _calculateCartesianCoordinates(AngularCoordinates angular_coordinates);
+
+  AngularDerivatives _calculateAngularDerivatives(AngularCoordinates angular_coordinates);
+
+  AngularCoordinates _calculateAngularCoordinates(PlaneCartesianCoordinates cartesian_coordinates);
+
+  double _calculateDeterminant(AngularDerivatives angular_derivatives);
+
+  void _moveByWithExactMethod(double delta_x, double delta_y);
+
+  void _moveByWithDerivativeMethod(double delta_x, double delta_y);
+
+  double _calculateHandAngle(AngularCoordinates angular_coordinates);
+
+  public:
+
+    RobotWithHorizontalHand(LoggingCallback logging_callback);
+    
+    PlaneCartesianCoordinates currentCartesianCoordinates();
+    
+    AngularCoordinates currentAngularCoordinates();
+    
+    void moveArmsTo(double shoulder_angle, double elbow_angle);
+    
+    void moveBy(double delta_x, double delta_y);
+    
+    void setMethodToExact();
+    
+    void setMethodToDerivative();
+
+};
 
 } // namespace robotic_arm
 
