d2a369ce50586f621ac2b7954e859b94f62f6143
alerest285 <81980735+alerest285@users.noreply.github.com>
2024-01-07 13:00:14 +0100

    Add functions definitions in math.cpp

--- src/math.cpp
+++ src/math.cpp
@@ -2,4 +2,28 @@
 
 namespace robotic_arm {
 
+double cosDegrees(double x){
+   return cos(x * pi / 180); 
+}
+
+double cosDegreesDerivative(double x){
+   return - (pi / 180) * sin(x * pi / 180); 
+}
+
+double sinDegrees(double x){
+  return sin(x * pi / 180);
+}
+
+double sinDegreesDerivative(double x){
+  return (pi / 180) * cos(x * pi / 180);
+}
+
+double acosDegrees(double x){
+  return acos(x) * 180 / pi;
+}
+
+PlaneCartesianCoordinates::toString(){
+    return "x: " + String(x) + ", y: " + String(y);
+}
+
 } // namespace robotic_arm
